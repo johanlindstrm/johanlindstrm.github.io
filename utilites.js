1a796b13baf441370963d7a621b589106a69bbad
johanlindstrom <johanlindstroom@outlook.com>
2020-05-09 23:58:46 +0200

    Index, utilities
    More responsive for mobile, projects now stacks and top nav is hidden when in mobile.

--- utilites.js
+++ utilites.js
@@ -18,39 +18,20 @@ var t = setInterval(function() {
   $('#changing').html( words[ randomNumber ] );
 }, 5000);
 
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
+
+
+  $( "#clickme" ).click(function() {
+    $( "#block" ).toggle( "slow", function() {
+      // Animation complete.
+    });
   });
+
+  $(document).click(function(){
+    $("button").toggle( "slow", function(){
+      $("#showProject").toggle();
+    });
+  });
+
+  $(function () {
+    $('#showProject').removeClass('hidden');
+});
